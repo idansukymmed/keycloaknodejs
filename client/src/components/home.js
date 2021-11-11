ee067f5903f624d2e0c58c58250ea922a1a944de
idansukymmed <d3mmyk@gmail.com>
2021-11-11 19:07:34 +0700

    logout button added

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import UserService from '../services/UserService';
+import React from "react";
+import UserService from "../services/UserService";
 
 const HomePage = () => {
-    return (
+  return (
+    <div>
+      <h1>Welcome to example page!</h1>
+      {UserService.isLoggedIn() ? (
         <div>
-            <h1>Welcome to example page!</h1>
-            <div>
-                <button
-                    onClick={() => UserService.doLogin()}
-                >
-                    Login
-                </button>
-            </div>
+          <p>Dont forget to logout</p>
+          <div>
+            <button onClick={() => UserService.doLogout()}>Logout</button>
+          </div>
         </div>
-    );
+      ) : (
+        <div>
+          <button onClick={() => UserService.doLogin()}>Login</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
